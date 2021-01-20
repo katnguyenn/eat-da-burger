067663675a51e081bbf318dc970296ad2902fbb1
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-01-20 12:36:51 -0800

    Created functions to interact with the database

--- models/burger.js
+++ models/burger.js
@@ -1,8 +1,29 @@
+// Import ORM to create functions that will interact with the database.
 const orm = require("../config/orm.js");
 
 
+const burger = {
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (res) {
+            cb(res);
+        });
+    },
+
+    insertOne: function (colToSearch, valOfCol, cb) {
+        orm.insertOne("burgers", colToSearch, valOfCol, function (res) {
+            cb(res);
+
+        });
+    },
+
+    updateOne: function (objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function (res) {
+            cb(res);
+        });
+    }
+
+};
 
 
-
-
+// Export the database functions for the controller
 module.exports = burger;
