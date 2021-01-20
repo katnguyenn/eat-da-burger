767b7f8a0caa1f1eedb960a019fe3df68436118e
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-01-20 11:18:03 -0800

    created methods that will execute nexcessary mysql commands in the controllers

--- config/orm.js
+++ config/orm.js
@@ -1,7 +1,32 @@
 const connection = require("./connection.js");
 
 
+const orm = {
+    selectAll: function (whatToSelect, tableInput, cb) {
+        const query = "SELECT ?? FROM ??";
+        connection.query(query, [whatToSelect, tableInput], (err, data) => {
+            if (err) throw err;
+            cb(data);
+        });
+    },
 
+    insertOne: function (tableInput, colToSearch, valOfCol, cb) {
+        const query = "INSERT INTO ?? ?? VALUES ?";
+        connection.query(query, [tableInput, colToSearch, valOfCol], (err, data) => {
+            if (err) throw err;
+            cb(data);
+        });
+    },
+
+    // An example of objColVals would be {name: panther, sleepy: true}
+    updateOne: function (tableInput, objColVals, condition, cb) {
+        const query = "UPDATE ?? SET ?? WHERE ?";
+        connection.query(query, [tableInput, objColVals, condition], (err, data) => {
+            if (err) throw err;
+            cb(data);
+        });
+    }
+}
 
 
 
