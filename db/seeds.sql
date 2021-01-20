b32f352ecda579e9e3669e3422ffb11d6f786a91
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-01-19 17:11:05 -0800

    DB Setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", true);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Cheeseburger", false);
