43285c8e0ba2c96dca839222064957fcc5131283
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-01-21 15:30:07 -0800

    Switched arrow function to regular functions and added helper functions

--- config/orm.js
+++ config/orm.js
@@ -1,28 +1,83 @@
 const connection = require("./connection.js");
 
+// Helper function to loop through and create array of question marks and turn it into a string
+const questionMarks = num => {
+    const arr = [];
+
+    for (let i = 0; i < num; i++) {
+        arr.push("?");
+    }
+
+    return arr.toString();
+}
+
+// Helper functon to convert object key/value pairs to SQL syntax
+
+const objToSql = ob => {
+    const arr = [];
+
+    // Loop through the keys and push pair as a string into arr
+    for (let key in ob) {
+        let value = ob[key];
+
+        if (Object.hasOwnProperty.call(ob, key)) {
+            // If string with spaces, add quotations
+            if (typeof value === "string" && value.indexOf(" ") >= 0) {
+                value = "'" + value + "'";
+            }
+
+            arr.push(key + "=" + value);
+        }
+    }
+    return arr.toString();
+}
 
 const orm = {
     selectAll: function (tableInput, cb) {
-        const query = "SELECT * FROM ??";
-        connection.query(query, tableInput, (err, data) => {
-            if (err) throw err;
+        const query = "SELECT * FROM " + tableInput + ";";
+        connection.query(query, function(err, data) {
+            if (err) {
+                throw err;
+            }
             cb(data);
         });
     },
 
     insertOne: function (tableInput, colToSearch, valOfCol, cb) {
-        const query = "INSERT INTO ?? ?? VALUES ?";
-        connection.query(query, [tableInput, colToSearch, valOfCol], (err, data) => {
-            if (err) throw err;
+        const query = "INSERT INTO " + tableInput;
+
+    query += " (";
+    query += colToSearch.toString();
+    query += ") ";
+    query += "VALUES (";
+    query += questionMarks(vals.length);
+    query += ") ";
+
+    console.log(query);
+
+        connection.query(query, valOfCol, function(err, data) {
+            if (err) {
+                throw err;
+            }
             cb(data);
         });
     },
 
     // An example of objColVals would be {name: panther, sleepy: true}
     updateOne: function (tableInput, objColVals, condition, cb) {
-        const query = "UPDATE ?? SET ?? WHERE ?";
-        connection.query(query, [tableInput, objColVals, condition], (err, data) => {
-            if (err) throw err;
+        const query = "UPDATE " + tableInput;
+
+    query += " SET ";
+    query += objToSql(objColVals);
+    query += " WHERE ";
+    query += condition;
+
+    console.log(query);
+
+        connection.query(query, function(err, data) {
+            if (err) {
+                throw err;
+            }
             cb(data);
         });
     }
