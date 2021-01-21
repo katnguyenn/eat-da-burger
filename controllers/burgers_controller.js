7374ff06322b537c72002f2ff49b878fcf1d70d4
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-01-20 16:15:01 -0800

    Created routes

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,2 +1,47 @@
 const express = require("express");
 const burger = require("../models/burger.js");
+const router = express.Router();
+
+
+
+// All routes
+router.get("/", (req, res) => {
+    burger.selectAll((data) => {
+        res.render("index", { burgers: data });
+    });
+
+});
+
+
+router.post("/api/burgers", (req, res) => {
+    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], result => {
+
+        res.json({ id: result.insertId });
+    });
+});
+
+
+router.put("/api/burgers/:id", (req, res) => {
+    const condition = "id: " + req.params.id;
+
+    burger.updateOne(
+        {
+            burger_name: req.body.burger_name
+        },
+        condition,
+        (err, result) => {
+            if (err) {
+                return res.status(500).end();
+            } else if (result.changedRows === 0) {
+                return res.status(404).end();
+            }
+            res.status(200).end();
+        }
+    );
+
+});
+
+
+
+// Express routes for server.js to use.
+module.exports = router;
