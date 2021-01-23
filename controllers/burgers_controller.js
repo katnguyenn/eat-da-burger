b8e974deda8d6f8214137fee4bb77a3673eda929
Katherine Nguyen <nguyenkat01@yahoo.com>
2021-01-22 21:49:56 -0800

    Removed err on put request and just sent 200 status

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const burger = require("../models/burger.js");
+const burger = require("../models/burger");
 const router = express.Router();
 
 
@@ -7,34 +7,38 @@ const router = express.Router();
 // All routes
 router.get("/", (req, res) => {
     burger.selectAll((data) => {
-        res.render("index", { burgers: data });
+        var dataObject = { // handlebars object
+            burgers: data
+        };
+        console.log(dataObject);
+        res.render("index", dataObject);
     });
 
 });
 
 
 router.post("/api/burgers", (req, res) => {
-    burger.insertOne(["burger_name", "devoured"], [req.body.burger_name, req.body.devoured], result => {
-
+    burger.insertOne(
+        ["burger_name", "devoured"], 
+        [req.body.burger_name, req.body.devoured], 
+        (result) => {
         res.json({ id: result.insertId });
     });
 });
 
 
 router.put("/api/burgers/:id", (req, res) => {
-    const condition = "id: " + req.params.id;
+    let condition = "id = " + req.params.id;
+
+    console.log("condition", condition);
 
     burger.updateOne(
         {
-            burger_name: req.body.burger_name
+            devoured: req.body.devoured
         },
         condition,
-        (err, result) => {
-            if (err) {
-                return res.status(500).end();
-            } else if (result.changedRows === 0) {
-                return res.status(404).end();
-            }
+        (result) => {
+            
             res.status(200).end();
         }
     );
